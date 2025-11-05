6479065ccd29b60effae36c6f8530a8d928c0a2a
vincentL233 <vincent02282010@gmail.com>
2025-11-05 20:36:26 +0800

    vueset

--- public/js/bs5vue.js
+++ public/js/bs5vue.js
@@ -26,26 +26,32 @@ const { createApp, ref } = Vue;
 var serviceApp=createApp({
     data(){
         return{
-            Services:  [
-                {icon:'fa-shopping-cart', title:'E-Commerce', text:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur porro laborum fuga repellat necessitatibus corporis nulla, in ex velit recusandae obcaecati maiores, doloremque quisquam similique, tempora aspernatur eligendi delectus! Rem.'},
-                {icon:'fa-laptop', title:'Responsive Design', text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.'},
-                {icon:'fa-lock', title:'Web Security', text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.'}
-            ]
+            Services:  []
         }
     }
 }).mount("#services");
 
-createApp({
+$.ajax({
+    url:"/services",
+    method: "get",
+    dataType: "json",
+    success: (result) =>{
+        serviceApp.Services = result;
+    }
+})
+
+var portfolioApp = createApp({
     setup(){
         return{
-            Portfolio: ref([
-                    { href: "#portfolioModal1", imgSrc: "img/portfolio/roundicons.png", title: "Round Icons", text: "Graphic Design" },
-                    { href: "#portfolioModal2", imgSrc: "img/portfolio/startup-framework.png", title: "Startup Framework", text: "Website Design" },
-                    { href: "#portfolioModal3", imgSrc: "img/portfolio/treehouse.png", title: "Treehouse", text: "Website Design" },
-                    { href: "#portfolioModal1", imgSrc: "img/portfolio/roundicons.png", title: "Round Icons", text: "Graphic Design" },
-                    { href: "#portfolioModal2", imgSrc: "img/portfolio/startup-framework.png", title: "Startup Framework", text: "Website Design" },
-                    { href: "#portfolioModal3", imgSrc: "img/portfolio/treehouse.png", title: "Treehouse", text: "Website Design" }
-                ])
+            Portfolio: ref([])
         }
     }
 }).mount("#portfolio");
+$.ajax({
+    url:"/portfolio",
+    method: "get",
+    dataType: "json",
+    success: (result) =>{
+        portfolioApp.Portfolio = result;
+    }
+})
--- server.js
+++ server.js
@@ -3,12 +3,31 @@ var bodyParser = require('body-parser');
 var server = express();
 
 server.use(express.static(__dirname + "/public"));
-server.use(bodyParser.urlencoded());
+server.use(bodyParser.urlencoded({extended: true}));
 server.get("/",(req,res)=>{
     res.send("Hello World");
 })
-
+server.get("/services",(req,res)=>{
+    var services = [
+        {icon:'fa-shopping-cart', title:'E-Commerce', text:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur porro laborum fuga repellat necessitatibus corporis nulla, in ex velit recusandae obcaecati maiores, doloremque quisquam similique, tempora aspernatur eligendi delectus! Rem.'},
+        {icon:'fa-laptop', title:'Responsive Design', text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.'},
+        {icon:'fa-lock', title:'Web Security', text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.'}
+     ];
+    res.json(services);
+});
 server.get("/about",(req,res)=>{
-    res.send("first server");
+    var user = req.query.user;
+    res.send("welcome back :" + user);
+});
+server.get("/portfolio",(req,res)=>{
+    var portfolio = [
+        { href: "#portfolioModal1", imgSrc: "img/portfolio/roundicons.png", title: "Round Icons", text: "Graphic Design" },
+        { href: "#portfolioModal2", imgSrc: "img/portfolio/startup-framework.png", title: "Startup Framework", text: "Website Design" },
+        { href: "#portfolioModal3", imgSrc: "img/portfolio/treehouse.png", title: "Treehouse", text: "Website Design" },
+        { href: "#portfolioModal1", imgSrc: "img/portfolio/roundicons.png", title: "Round Icons", text: "Graphic Design" },
+        { href: "#portfolioModal2", imgSrc: "img/portfolio/startup-framework.png", title: "Startup Framework", text: "Website Design" },
+        { href: "#portfolioModal3", imgSrc: "img/portfolio/treehouse.png", title: "Treehouse", text: "Website Design" }
+    ];
+    res.json(portfolio);
 });
 server.listen(8080);
