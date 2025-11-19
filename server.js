d282c0d1d91e9d722d589d8daf28c0f729125461
vincentL233 <vincent02282010@gmail.com>
2025-11-19 20:07:31 +0800

    vcv

--- server.js
+++ server.js
@@ -2,32 +2,74 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var server = express();
 
+var DB=require("nedb-promises");
+var ServerDB = DB.create(__dirname + "/Service.db");
+var portfolioDB = DB.create(__dirname + "/portfolio.db");
+var contactDB = DB.create(__dirname + "/contact.db");
+// ServerDB.insert([
+    
+//     {icon:'fa-shopping-cart', title:'E-Commerce', text:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur porro laborum fuga repellat necessitatibus corporis nulla, in ex velit recusandae obcaecati maiores, doloremque quisquam similique, tempora aspernatur eligendi delectus! Rem.'},
+//     {icon:'fa-laptop', title:'Responsive Design', text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.'},
+//     {icon:'fa-lock', title:'Web Security', text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.'},
+// ]);
+
+// portfolioDB.insert([
+//     { href: "#portfolioModal1", imgSrc: "img/portfolio/roundicons.png", title: "Round Icons", text: "Graphic Design" },
+//         { href: "#portfolioModal2", imgSrc: "img/portfolio/startup-framework.png", title: "Startup Framework", text: "Website Design" },
+//         { href: "#portfolioModal3", imgSrc: "img/portfolio/treehouse.png", title: "Treehouse", text: "Website Design" },
+//         { href: "#portfolioModal1", imgSrc: "img/portfolio/roundicons.png", title: "Round Icons", text: "Graphic Design" },
+//         { href: "#portfolioModal2", imgSrc: "img/portfolio/startup-framework.png", title: "Startup Framework", text: "Website Design" },
+//         { href: "#portfolioModal3", imgSrc: "img/portfolio/treehouse.png", title: "Treehouse", text: "Website Design" }
+// ]);
+
 server.use(express.static(__dirname + "/public"));
 server.use(bodyParser.urlencoded({extended: true}));
 server.get("/",(req,res)=>{
     res.send("Hello World");
 })
 server.get("/services",(req,res)=>{
-    var services = [
-        {icon:'fa-shopping-cart', title:'E-Commerce', text:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur porro laborum fuga repellat necessitatibus corporis nulla, in ex velit recusandae obcaecati maiores, doloremque quisquam similique, tempora aspernatur eligendi delectus! Rem.'},
-        {icon:'fa-laptop', title:'Responsive Design', text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.'},
-        {icon:'fa-lock', title:'Web Security', text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.'}
-     ];
-    res.json(services);
+    // var services = [
+    //     {icon:'fa-shopping-cart', title:'E-Commerce', text:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur porro laborum fuga repellat necessitatibus corporis nulla, in ex velit recusandae obcaecati maiores, doloremque quisquam similique, tempora aspernatur eligendi delectus! Rem.'},
+    //     {icon:'fa-laptop', title:'Responsive Design', text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.'},
+    //     {icon:'fa-lock', title:'Web Security', text:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.'}
+    //  ];
+    ServerDB.find({},{"_id":0}).then(results=>{
+        res.json(results);
+    }).catch(err=>{
+        console.log(err);
+    })
 });
 server.get("/about",(req,res)=>{
     var user = req.query.user;
     res.send("welcome back :" + user);
 });
 server.get("/portfolio",(req,res)=>{
-    var portfolio = [
-        { href: "#portfolioModal1", imgSrc: "img/portfolio/roundicons.png", title: "Round Icons", text: "Graphic Design" },
-        { href: "#portfolioModal2", imgSrc: "img/portfolio/startup-framework.png", title: "Startup Framework", text: "Website Design" },
-        { href: "#portfolioModal3", imgSrc: "img/portfolio/treehouse.png", title: "Treehouse", text: "Website Design" },
-        { href: "#portfolioModal1", imgSrc: "img/portfolio/roundicons.png", title: "Round Icons", text: "Graphic Design" },
-        { href: "#portfolioModal2", imgSrc: "img/portfolio/startup-framework.png", title: "Startup Framework", text: "Website Design" },
-        { href: "#portfolioModal3", imgSrc: "img/portfolio/treehouse.png", title: "Treehouse", text: "Website Design" }
-    ];
-    res.json(portfolio);
+    // var portfolio = [
+    //     { href: "#portfolioModal1", imgSrc: "img/portfolio/roundicons.png", title: "Round Icons", text: "Graphic Design" },
+    //     { href: "#portfolioModal2", imgSrc: "img/portfolio/startup-framework.png", title: "Startup Framework", text: "Website Design" },
+    //     { href: "#portfolioModal3", imgSrc: "img/portfolio/treehouse.png", title: "Treehouse", text: "Website Design" },
+    //     { href: "#portfolioModal1", imgSrc: "img/portfolio/roundicons.png", title: "Round Icons", text: "Graphic Design" },
+    //     { href: "#portfolioModal2", imgSrc: "img/portfolio/startup-framework.png", title: "Startup Framework", text: "Website Design" },
+    //     { href: "#portfolioModal3", imgSrc: "img/portfolio/treehouse.png", title: "Treehouse", text: "Website Design" }
+    // ];
+    // res.json(portfolio);
+    portfolioDB.find({},{"_id":0}).then(results=>{
+        res.json(results);
+    }).catch(err=>{
+        console.log(err);
+    })
+}); 
+
+server.post("/contact",(req,res)=>{
+    // var name = req.body.name;
+    // var email = req.body.email;
+    // var phone = req.body.phone;
+    // var message = req.body.message;
+    // console.log(`name: ${name}, email: ${email}, phone: ${phone}, message: ${message}`);
+    // res.send("Thank you for contacting us!");
+    contactDB.insert(req.body).then(()=>{
+        res.send("Thank you for contacting us!");
+    })
 });
+
 server.listen(8080);
